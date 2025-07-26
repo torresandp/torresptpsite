3d776b2c6b012ac3fdb93788104c00285c55a8ca
Richard Torres <richard.torres@torresptp.com>
2025-07-26 11:44:23 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const pacifico = Pacifico({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-pacifico',
-})
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Torres & P. PTP LLC",
-  description: "Torres & P. All right reserved.",
+  description: "Torres & P. All rights reserved.",
 };
 
 export default function RootLayout({
@@ -35,7 +37,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} antialiased`}
       >
-        {children}
+        <Navigation />
+        <main>{children}</main>
+        <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
