b15d25ff4936931149b159d3a3e0fb3dac6fddb7
Richard Torres <richard.torres@torresptp.com>
2025-07-26 02:13:18 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const pacifico = Pacifico({
   weight: '400',
